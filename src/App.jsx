cfa80c8416e1e724fda69d4d153685404ab8ac37
Shaoting-Yan <112460516+Shaoting-Yan@users.noreply.github.com>
2023-10-09 11:41:09 +0800

    fixed minor issues

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import "./App.css";
 import {useAnimate} from "framer-motion";
 import {useDeviceOrientation } from './components/useDeviceOrientation';
 import {useVolumeLevel, VolumeIndicator} from 'react-volume-indicator';
+import ReactGA from 'react-ga';
 
 function App() {
   const { orientation,motion, requestAccess, revokeAccess, error } = useDeviceOrientation();
@@ -18,6 +19,9 @@ function App() {
   const [loading,setLoading] = useState(true);
   const [choosing,setChoosing] = useState(false);
   
+  const TRACKING_ID = "G-K853SF4HV7"; // OUR_TRACKING_ID
+  ReactGA.initialize(TRACKING_ID);
+
   const handleChange = (index)=>{
     setCurrIndex(index);
   }
@@ -36,13 +40,19 @@ function App() {
   const handleLoadDone = ()=>{
     setLoading(false);
   }
+  const handleClick = ()=>{
+    ReactGA.event({
+      category: 'Interaction',
+      action: 'All'
+    });
+  }
   return (
     <>{loading?
         <div className="u-absolute u-layerTop u-fill">
           <Preloader loadDone={handleLoadDone} getPermission={requestAccess}/>
         </div>
       :<></>}
-      <div className="u-relative viewport">
+      <div className="u-relative viewport" onClick={handleClick}>
         <TopBar title={themeList[currIndex]}/>
         <div className="u-absolute u-fill u-layer0 middle-container">
           <div className="u-relative u-fill">
